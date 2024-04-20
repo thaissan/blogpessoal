6956a5dc35044fd99a85d14002e736a60f4d288f
thaissan <thais.cavadas@hotmail.com>
2024-04-19 22:33:22 -0300

    add TemaService e TemaModule no PostagemModule

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Postagem } from "./entities/postagem.entity";
 import { PostagemService } from "./services/postagem.service";
 import { PostagemController } from "./controllers/postagem.controller";
-
+import { TemaModule } from "../tema/tema.module";
+import { TemaService } from "../tema/services/tema.service";
 @Module({
-    imports: [TypeOrmModule.forFeature([Postagem])],
-    providers: [PostagemService],
+    imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
+    providers: [PostagemService, TemaService],
     controllers: [PostagemController],
     exports:[TypeOrmModule]
 })
